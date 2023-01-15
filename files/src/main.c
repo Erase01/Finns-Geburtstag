be0ef8367bd41872c0895ff7e611a82674d2477e
hyperbel <hyperbel@users.noreply.github.com>
2023-01-15 15:39:27 +0100

    c program for strings thing

---
+++ files/src/main.c
@@ -0,0 +1,7 @@
+int main() {
+  return 0;
+}
+
+void FLAGSTART___UVVJWl9TVFJJTkdTX0lVUUVSSExLSk5MVkNKRUkK___FLAGEND() {
+  return;
+}
--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
 	r.LoadHTMLGlob("sites/*.html")
 
 	r.GET("/", index)
+	r.GET("/quiz", quiz)
+	r.GET("/rdr2", rdr2)
+
 	r.Run()
 }
 
@@ -19,4 +22,12 @@ func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"text": "moin",
 	})
+}
+
+func quiz(c *gin.Context) {
+	c.HTML(http.StatusOK, "quiz.html", gin.H{})
+}
+
+func rdr2(c *gin.Context) {
+	c.HTML(http.StatusOK, "rdr2.html", gin.H{})
 }
