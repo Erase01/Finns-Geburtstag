b5ea0bb8f7e49192a78f6bf3b9dc8b18eda07434
hyperbel <hyperbel@users.noreply.github.com>
2023-01-19 21:15:06 +0100

    flag check branching with if statements

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 
-	fmt.Printf("%x\n", string((get_hash([]byte("test")))))
+	// 73c46df076e245e59cfe4e3d362b0c2c is the hash of the `strings` command flag
 	r := gin.Default()
 
 	r.Static("/assets", "./assets")
@@ -40,7 +40,13 @@ func rdr2(c *gin.Context) {
 func submit_flag(c *gin.Context) {
 	flag := c.Query("flag")
 	fmt.Println(flag)
-	c.HTML(http.StatusPermanentRedirect, "index.html", gin.H{"msg": "flag richtig!"})
+	hash_of_flag := string((get_hash([]byte(flag))))
+	fmt.Println(hash_of_flag)
+	if flag == "73c46df076e245e59cfe4e3d362b0c2c" {
+		c.HTML(http.StatusPermanentRedirect, "index.html", gin.H{"quiz": "Ja!"})
+	} else {
+		c.HTML(http.StatusPermanentRedirect, "index.html", gin.H{})
+	}
 }
 
 func download(c *gin.Context) {
