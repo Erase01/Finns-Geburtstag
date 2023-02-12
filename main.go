4ae02e5ec3dc48ff812514c6ba830e287380f280
Erase01 <lucajulien16@gmx.de>
2023-02-12 10:47:12 +0100

    dsfds

--- main.go
+++ main.go
@@ -155,12 +155,12 @@ func check_image_click(x int, y int, index int) float64 {
 	const MAX_DISTANCE = 50 // max distance between click and wanted pos
 
 	fmt.Println(x, y)
+	fmt.Println(index)
 	switch i := index; i {
 	case 0:
 		x2, y2 = 1088, 656
 	case 1:
 		x2, y2 = 1030, 385
-		fmt.Println(x2, y2)
 	case 2:
 		x2, y2 = 1060, 370
 	case 3:
